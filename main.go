a89ae808510a610926106e39f87678c5bfa1db13
muhammedaliakkaya <muh.ali.akkaya@gmail.com>
2021-05-07 13:03:34 +0300

    v3

---
+++ main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+type User struct {
+	Name     string
+	Age      int
+	Birthday time.Time
+}
+
+func main() {
+	var user []User
+
+	db.Find(&user)  //db deki verileri getirme işlemi yapıyor
+	db.First(&user) // db deki ilk eşleşen veriyi gettirir
+	for index, usercik := range user {
+		fmt.Println(index, usercik)
+	}
+	fmt.Println(user)
+}
